46df98dfa31072af101668d2e33e7300c970e7cf
ananjaser1211 <ananjaser@gmail.com>
2018-01-12 01:19:12 +0400

    Revert "Remove samsung hard enforce"
    This reverts commit fc6568160815f81bedf8cec4e55448addce50c82.

--- security/selinux_n/ss/services.c
+++ security/selinux_n/ss/services.c
@@ -772,6 +772,11 @@ out:
 	kfree(n);
 	kfree(t);
 
+// [ SEC_SELINUX_PORTING_COMMON
+#ifdef CONFIG_ALWAYS_ENFORCE
+	selinux_enforcing = 1;
+#endif
+// ] SEC_SELINUX_PORTING_COMMON
 	if (!selinux_enforcing)
 		return 0;
 	return -EPERM;
@@ -1531,6 +1536,11 @@ out:
 	kfree(t);
 	kfree(n);
 
+// [ SEC_SELINUX_PORTING_COMMON
+#ifdef CONFIG_ALWAYS_ENFORCE
+	selinux_enforcing = 1;
+#endif
+// ] SEC_SELINUX_PORTING_COMMON
 	if (!selinux_enforcing)
 		return 0;
 	return -EACCES;
@@ -1822,6 +1832,11 @@ static inline int convert_context_handle_invalid_context(struct context *context
 	char *s;
 	u32 len;
 
+// [ SEC_SELINUX_PORTING_COMMON
+#ifdef CONFIG_ALWAYS_ENFORCE
+	selinux_enforcing = 1;
+#endif
+// ] SEC_SELINUX_PORTING_COMMON
 	if (selinux_enforcing)
 		return -EINVAL;
 
